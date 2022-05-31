60a7a39fe6b2f948744028fe0236fed43ebdaf62
hosein alibandehloo <76466142+hosein6222@users.noreply.github.com>
2022-05-31 11:51:09 +0430

    Update st.py

--- st.py
+++ st.py
@@ -9,7 +9,7 @@ bp = BP2.BP_Updater()
 #     bp.update(values[0], values[1])
 
 
-@st.cache(suppress_st_warning=True)
+@st.cache(suppress_st_warning=True, show_spinner=False)
 def onClickUpdate():
 #     st.write('Dabase is updating...')
     bp.update(starting_row, ending_row)
