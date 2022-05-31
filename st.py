f626f6fa607d3ced61a3092224f0ceb28456db81
hosein alibandehloo <76466142+hosein6222@users.noreply.github.com>
2022-05-31 09:31:22 +0430

    Update st.py

--- st.py
+++ st.py
@@ -8,11 +8,14 @@ def onClickUpdate():
     st.write('Values:', values)
     bp.update(values[0], values[1])
 
-st.title('Macaan')
+st.title('BP Database Updater')
+
+starting_row = st.number_input('Insert starting row number', min_value=2, help='You need to starting row of your database table')
+ending_row = st.number_input('Insert ending row number', max_value=3000)
 
 values = st.slider(
      'Select a range',
      2, 100, (2, 100), step=1)
 
 
-updateButton = st.button('Update', on_click=onClickUpdate)
+updateButton = st.button('Update Date', on_click=onClickUpdate)
