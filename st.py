51c4befd36d08bf4029198b52a17a80e874cc5d7
hosein alibandehloo <76466142+hosein6222@users.noreply.github.com>
2022-06-27 16:00:13 +0430

    Update st.py

--- st.py
+++ st.py
@@ -16,7 +16,7 @@ bp = BP2.BP_Updater()
 # page_type = st.sidebar.button('Business page')
 with st.sidebar:
     page_type = st.sidebar.selectbox(
-    "How would you like to be updated?",
+    "Which would you like to update?",
     ("Influencer", "Business page", "Telegram", 'PR')
 )
     st.write(page_type, 'is selected.')
@@ -24,25 +24,25 @@ with st.sidebar:
 
      
 st.title('Database Updater')
-with st.container():
-    if page_type == 'Influencer':
-         st.write('You selected Influencer.')
-         st.dataframe(bp.get_data('Contact influencer'))
-    else:
-         st.write("You selected Business page.")
-         st.dataframe(bp.get_data('contact business page'))
+if page_type == 'Influencer':
+     st.write('You selected Influencer.')
+     st.dataframe(bp.get_data('Contact influencer'))
+else:
+     st.write("You selected Business page.")
+     st.dataframe(bp.get_data('contact business page'))
 
-    starting_row = st.number_input('Insert starting row number', min_value=2, help='You need to enter starting row of your database table')
-    ending_row = st.number_input('Insert ending row number', min_value = starting_row + 1, max_value=10000)
+starting_row = st.number_input('Insert starting row number', min_value=2, help='You need to enter starting row of your database table')
+ending_row = st.number_input('Insert ending row number', min_value = starting_row + 1, max_value=10000)
 
-    # values = st.slider(
-    #      'Select a range',
-    #      2, 100, (2, 100), step=1)
+# values = st.slider(
+#      'Select a range',
+#      2, 100, (2, 100), step=1)
 
 
-    @st.cache(suppress_st_warning=True)
-    def onClickUpdate():
-         bp.update(starting_row, ending_row, st, page_type)
+@st.cache(suppress_st_warning=True)
+def onClickUpdate():
+     bp.update(starting_row, ending_row, st, page_type)
+     st.sucess('Process Done!')
 
-    updateButton = st.button('Update Date', on_click=onClickUpdate)
-    # st.write('Done!') 
+updateButton = st.button('Update Date', on_click=onClickUpdate)
+# st.write('Done!') 
