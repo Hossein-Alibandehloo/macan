da6687f13ebcbd128e5fe47a33123b2aba075006
hosein alibandehloo <76466142+hosein6222@users.noreply.github.com>
2022-07-03 12:18:23 +0430

    Update BP2.py

--- BP2.py
+++ BP2.py
@@ -37,6 +37,7 @@ class BP_Updater:
         
 
     def influencermarketinghub(self, id):
+        print(id)
         with requests.Session() as s:
             header = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',}
             Form_data = {
