8620f4574c3fd7b52aec178ae3e17e2d20ca5aec
hosein alibandehloo <76466142+hosein6222@users.noreply.github.com>
2023-01-28 19:11:15 +0330

    Update BP2.py

--- BP2.py
+++ BP2.py
@@ -161,7 +161,10 @@ class BP_Updater:
 #         index = [first[0] for first in data][1:]
         headless_data = data[1:]
         df = pd.DataFrame(headless_data, columns=data[0])
-        del df["Phone"]
+        try:
+            del df["Phone"]
+        except:
+            pass
         if 'PhoneNumber' in data[0]:
             del df['PhoneNumber']
         
